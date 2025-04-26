eef9f07e719b8061c3572b3a755a0dec1c96c132
Luis Martin Mola Enriquez <molaenriquezl@gmail.com>
2025-04-26 09:50:26 -0400

    cabio

--- src/Components/Utils/LoadingScreen/LoadingScreen.jsx
+++ src/Components/Utils/LoadingScreen/LoadingScreen.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import Logo from "../../../assets/Logo.png";
+import Logo from "./../../../assets/Logo.png";
 import "./LoadingScreen.css";
 
 const LoadingScreen = () => {
