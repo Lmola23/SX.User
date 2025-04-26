c735d7b108499ca27a3a782a8ed3499fc0f5f1a4
Luis Martin Mola Enriquez <molaenriquezl@gmail.com>
2025-04-26 09:53:01 -0400

    .....

--- src/Components/Utils/LoadingScreen/LoadingScreen.jsx
+++ src/Components/Utils/LoadingScreen/LoadingScreen.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import Logo from "./../../../assets/Logo.png";
+import Logo from "./../../../assets/logo.png";
 import "./LoadingScreen.css";
 
 const LoadingScreen = () => {
