a59a1820189c62aaf4235d6159b5e3d6e227074a
Luis Martin Mola Enriquez <molaenriquezl@gmail.com>
2025-05-18 17:13:15 -0400

    ImplementadoLazylonding
    mejorando los tiempos de carga de la aplicacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Suspense } from 'react';
+import { Suspense , lazy } from 'react';
 import Navbar from "./Components/Navbar/navbar.jsx";
-import { Home, Servicio, Producto, Reservacion, Perfil } from "./Pages/index.js";
-import { Login, Register } from './Components/Account/index.js';
 import { AuthProvider } from './Components/Utils/AuthProvider/AuthProvider.jsx';
 import { ProtectedRoute, ScrollToTop } from './Components/Utils/index.js';
 import Footer from './Components/Footer/footer.jsx';
 import { requestNotificationPermission, onMessageListener } from "./firebaseconf.js";
 import { useEffect } from "react";
 import LoadingScreen from './Components/Utils/LoadingScreen/LoadingScreen';
+
+// Importa los componentes de forma lazy
+const Home = lazy(() => import('./Pages/Home/home.jsx'));
+const Servicio = lazy(() => import('./Pages/Servicios/Servicio.jsx'));
+const Producto = lazy(() => import('./Pages/Productos/Producto.jsx'));
+const Reservacion = lazy(() => import('./Pages/Reservaciones/Reservacion.jsx'));
+const Perfil = lazy(() => import('./Pages/Perfil/Perfil.jsx'));
+const Login = lazy(() => import('./Components/Account/Login/Login.jsx'));
+const Register = lazy(() => import('./Components/Account/Register/Register.jsx'));
 
 function App() {
   useEffect(() => {
